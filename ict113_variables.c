ae5d19b6cff7d06891a7cf0942f43230ad1dc94b
Bright Simbeya <simbeyabright0@gmail.com>
2025-12-25 21:51:03 +0200

    Add ict113_variables program

---
+++ ict113_variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+
+    char name[] = "Bright Simbeya";   // string variable
+
+    int age = 23;                  // integer variable
+
+    float gpa = 3.8;             // float variable
+
+    printf("Name: %s\n", name);
+
+    printf("Age: %d\n", age);
+
+    printf("GPA: %.1f\n", gpa);
+
+    return 0;
+}
